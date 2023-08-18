cb62145ad0fd6ffee021ce0aa037b0446782e0f1
Sagar Kaushik <66814609+Sagar-30@users.noreply.github.com>
2023-08-18 17:28:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+window.addEventListener('load', () => {
+const mainPrank = document.getElementById('Prank');
+   
+  setTimeout(()=>{
+
+       mainPrank.style.top='5vh';
+  },3000);
+console.log('Clicked');
+});
+
